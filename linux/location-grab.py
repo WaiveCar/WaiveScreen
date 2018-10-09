449b6f9ad2e024c664a608f90833c4f47e3ebea0
Chris McKenzie <kristopolous@yahoo.com>
2018-10-08 18:44:39 -0700

    dbus iface sniffing

--- linux/location-grab.py
+++ linux/location-grab.py
@@ -5,21 +5,35 @@ import time
 import pprint
 
 bus = dbus.SystemBus()
-proxy = bus.get_object('org.freedesktop.ModemManager1','/org/freedesktop/ModemManager1/Modem/1')
-iface = {
-    'location': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Location'),
-    'time': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Time')
-}
 
 old_lat = False
 old_lng = False
 ix = 0
 
+modem = 0
+def next_iface():
+    global modem
+    print("Trying modem {}".format(modem))
+    proxy = bus.get_object('org.freedesktop.ModemManager1','/org/freedesktop/ModemManager1/Modem/{}'.format(modem))
+    iface = {
+        'location': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Location'),
+        'time': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Time')
+    }
+    modem += 1 
+    return iface
+
+iface = next_iface()
+
 while True:
-    location = iface['location'].GetLocation()
-    pprint.pprint(location) 
-    location = iface['time'].GetNetworkTime()
-    pprint.pprint(location) 
+    try: 
+        location = iface['location'].GetLocation()
+        pprint.pprint(location) 
+        location = iface['time'].GetNetworkTime()
+        pprint.pprint(location) 
+
+    except:
+        iface = next_iface()
+
     """
     lat = "{:.5f}".format(location[2]['latitude'])
     lng = "{:.5f}".format(location[2]['longitude'])
