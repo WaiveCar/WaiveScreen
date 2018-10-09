9631f89248a66bdee1042fa1d37ce04196ddb360
Chris McKenzie <kristopolous@yahoo.com>
2018-10-08 18:27:15 -0700

    updating the location lib

--- linux/location-grab.py
+++ linux/location-grab.py
@@ -2,17 +2,25 @@
 
 import dbus
 import time
+import pprint
 
 bus = dbus.SystemBus()
-proxy = bus.get_object('org.freedesktop.ModemManager1','/org/freedesktop/ModemManager1/Modem/0')
-iface = dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Location')
+proxy = bus.get_object('org.freedesktop.ModemManager1','/org/freedesktop/ModemManager1/Modem/1')
+iface = {
+    'location': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Location'),
+    'time': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Time')
+}
 
 old_lat = False
 old_lng = False
 ix = 0
 
 while True:
-    location = iface.GetLocation()
+    location = iface['location'].GetLocation()
+    pprint.pprint(location) 
+    location = iface['time'].GetNetworkTime()
+    pprint.pprint(location) 
+    """
     lat = "{:.5f}".format(location[2]['latitude'])
     lng = "{:.5f}".format(location[2]['longitude'])
     ix += 1
@@ -24,4 +32,5 @@ while True:
 
     old_lat = lat
     old_lng = lng
+    """
     time.sleep(2)
