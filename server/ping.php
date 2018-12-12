8d0314ee20bedb87fb67de5eb5512c4cea62bf31
Chris McKenzie <kristopolous@yahoo.com>
2018-12-12 05:03:53 -0800

    Updating ping system

--- server/ping.php
+++ server/ping.php
@@ -10,17 +10,17 @@ $db = new SQLite3("main.db", SQLITE3_OPEN_READWRITE);
 
 $uid = $db->escapeString($_GET['uid']);
 
-$row = $db->querySingle("select * from lookup where uid='$uid'", true);
+$row = $db->querySingle("select * from screen where uid='$uid'", true);
 
 if(!$row) {
   // we need to get the next available port number
-  $nextport = intval($db->querySingle('select max(port) from lookup')) + 1;
+  $nextport = intval($db->querySingle('select max(port) from screen')) + 1;
 
-  $db->query("insert into lookup(uid, port, first_seen, last_seen) values('$uid', $nextport, current_timestamp, current_timestamp)");
+  $db->query("insert into screen(uid, port, first_seen, last_seen) values('$uid', $nextport, current_timestamp, current_timestamp)");
   echo $nextport + $PORT_OFFSET;
   exit(0);
 }
 
-$db->query('update lookup set last_seen=current_timestamp where id=' . $row['id']);
+$db->query('update screen set last_seen=current_timestamp where id=' . $row['id']);
 echo $row['port'] + $PORT_OFFSET;
 exit(0);
