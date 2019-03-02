5d0c1306bb1f086b9f446433583f7932c6f77938
Chris McKenzie <kristopolous@yahoo.com>
2019-03-02 10:39:09 -0800

    refactoring arduino part of the codebase

---
+++ ScreenDaemon/lib/lib.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import db
+
+
+"""
+  1. grabs data from the board, puts it in database
+  2. serves things to screen display, getting it from the database
+  3. talks to ad daemon using most recent data from database for lat/lng
+"""
+
+def sensor_store(data):
+    pass
+
+def sensor_last(index = 0):
+    pass
+
+def job_store(data):
+    pass
+
+def job_get(index = 0):
+    pass
