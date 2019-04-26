21c7448afb1a52d6a94a98056b17966b83c450c2
Chris McKenzie <kristopolous@yahoo.com>
2019-04-25 19:33:52 -0700

    miscellaneous updates

--- Linux/fai-config/files/home/setup.sh
+++ Linux/fai-config/files/home/setup.sh
@@ -6,3 +6,7 @@ else
   git clone git@github.com:WaiveCar/WaiveScreen.git
 fi
 
+cd WaiveScreen/ScreenDaemom
+pip3 install -r requirements.txt 
+ainsl ~demo/.bashrc 'PATH=$PATH:$HOME/.local/bin' 'HOME/.local/bin'
+./ScreenDaemon.py &
