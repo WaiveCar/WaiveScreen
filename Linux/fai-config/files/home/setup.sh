164325babf03a028df68f76dba7dd2a542e7ea6b
Chris McKenzie <kristopolous@yahoo.com>
2019-04-25 17:40:08 -0700

    Making the user-script more idempotent

--- Linux/fai-config/files/home/setup.sh
+++ Linux/fai-config/files/home/setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-git clone git@github.com:WaiveCar/WaiveScreen.git
+if [ -e WaiveScreen ]; then
+  cd WaiveScreen
+  git pull
+else  
+  git clone git@github.com:WaiveCar/WaiveScreen.git
+fi
+
