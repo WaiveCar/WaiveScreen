d4778a849e80056b7cda2f85383185b096341509
Chris McKenzie <kristopolous@yahoo.com>
2019-05-03 14:23:24 -0700

    Making sure that the chromium loop doesn't go crazy

--- Linux/fai-config/files/home/start-x-stuff.sh
+++ Linux/fai-config/files/home/start-x-stuff.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+count=`pgrep start-x-stuff | wc -l`
+if [ "$count" -gt "2" ]; then
+  exit -1
+fi
+
 export DISPLAY=$1
 /usr/bin/notion &
 [ -e ~demo/screen-splash.png ] && /usr/bin/display -window root ~demo/screen-splash.png
 while [ 0 ]; do
   /usr/bin/chromium --app=file:///home/demo/WaiveScreen/ScreenDisplay/display.html
-  sleep 1
+
+  count=`pgrep chromium | wc -l`
+  while [ "$count" -ne "0" ]; do
+    count=`pgrep chromium | wc -l`
+    sleep 5
+  done
+  count=`pgrep Xorg | wc -l`
+  if [ "$count" -eq "0" ]; then
+    exit
+  fi
 done
