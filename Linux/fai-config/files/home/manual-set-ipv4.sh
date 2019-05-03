81aba80989fc4c104dbf251ef4db0bfc864fadd2
Chris McKenzie <kristopolous@yahoo.com>
2019-05-03 14:23:58 -0700

    trying to simplify how the network is being done

--- Linux/fai-config/files/home/manual-set-ipv4.sh
+++ Linux/fai-config/files/home/manual-set-ipv4.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-sudo mmcli -m 0 -e
+set -x
+export PATH=$PATH:/usr/bin/
 
-sudo mmcli -m 0 \
+sleep 5
+
+mmcli -m 0 -e
+
+mmcli -m 0 \
 	--location-enable-gps-raw \
 	--location-enable-gps-nmea \
 	--location-set-enable-signal
 
-sudo mmcli -m 0 --simple-connect="apn=internet"
+mmcli -m 0 --simple-connect="apn=internet"
 wwan=`ip addr show | grep wwp | head -1 | awk -F ':' ' { print $2 } '`
 
 if [ -z "$wwan" ]; then
@@ -16,17 +21,19 @@ if [ -z "$wwan" ]; then
 fi
 
 # get ipv6
-sudo dhclient $wwan
+dhclient $wwan &
 
 # Show the config | find ipv4 | drop the LHS | replace the colons with equals | drop the whitespace | put everything on one line
 eval `mmcli -b 0 | grep -A 3 IPv4| awk -F '|' ' { print $2 } ' | sed s'/: /=/' | sed -E s'/\s+//' | tr '\n' ';'`
 
-sudo ip addr add $address/$prefix  dev $wwan
-sudo ip route add default via $gateway dev $wwan
+ip addr add $address/$prefix  dev $wwan
+ip route add default via $gateway dev $wwan
 
-cat << ENDL | sudo tee /etc/resolv.conf
+cat << ENDL | tee /etc/resolv.conf
 nameserver 8.8.8.8
 nameserver 4.2.2.1
 nameserver 2001:4860:4860::8888 
 nameserver 2001:4860:4860::8844
 ENDL
+
+/home/demo/dev-setup.sh
