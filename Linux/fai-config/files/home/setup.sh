b7adc59b698fd6aa84a83537de91669794dad52c
Chris McKenzie <kristopolous@yahoo.com>
2019-05-03 19:05:19 -0700

    Organizing te screen setup code

--- Linux/fai-config/files/home/setup.sh
+++ Linux/fai-config/files/home/setup.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-if [ -e WaiveScreen ]; then
-  cd WaiveScreen
-  git pull
-else  
-  git clone git@github.com:WaiveCar/WaiveScreen.git
-  ainsl ~demo/.bashrc 'PATH=$PATH:$HOME/.local/bin' 'HOME/.local/bin'
-fi
+git() {
+  if [ -e WaiveScreen ]; then
+    cd WaiveScreen
+    git pull
+  else  
+    git clone git@github.com:WaiveCar/WaiveScreen.git
+    ainsl ~demo/.bashrc 'PATH=$PATH:$HOME/.local/bin' 'HOME/.local/bin'
+  fi
+}
 
-UUID=/etc/UUID
-if [ ! -e $UUID ] ; then
-  sudo dmidecode -t 4 | grep ID | sed -E s'/ID://;s/\s//g' | sudo tee $UUID
-fi
+uuid() {
+  UUID=/etc/UUID
+  if [ ! -e $UUID ] ; then
+    sudo dmidecode -t 4 | grep ID | sed -E s'/ID://;s/\s//g' | sudo tee $UUID
+  fi
+}
 
-cd WaiveScreen/ScreenDaemon
-pip3 install -r requirements.txt 
-#./ScreenDaemon.py &
+install() {
+  cd WaiveScreen/ScreenDaemon
+  pip3 install -r requirements.txt 
+}
+
+run() {
+  cd $HOME/WaiveScreen/ScreenDaemon
+  ./ScreenDaemon.py&
+}
+
+# git
+# install
+
+uuid
+run
