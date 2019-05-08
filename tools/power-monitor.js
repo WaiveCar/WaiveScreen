5c883efb55fab7bb9f50d2f4f15c20bf3ac7496b
Chris McKenzie <kristopolous@yahoo.com>
2019-05-07 17:16:48 -0700

    Adding a tool to do power consumption monitoring with gnuplot

---
+++ tools/power-monitor.js
@@ -0,0 +1,22 @@
+const { Client } = require('tplink-smarthome-api');
+let fs = require('fs');
+
+const client = new Client();
+
+client.on('plug-new', function (plug) {
+  function reader() {
+    plug.emeter.getRealtime().then((opts) => {
+      delete opts['voltage'];
+      console.log( Object.values(opts).join(' ') );
+      setTimeout(reader, 750);
+    });
+  }
+  reader();
+});
+
+// Look for devices, log to console, and turn them on
+client.startDiscovery().on('device-new', (device) => {
+  device.setPowerState(true);
+});
+
+
