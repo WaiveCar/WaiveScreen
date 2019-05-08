5c9fde0c67222b8d99280e302bd4cd3562d76985
Chris McKenzie <kristopolous@yahoo.com>
2019-05-07 20:50:48 -0700

    Allowing root into the sshfs mount

--- Linux/fai-config/files/home/dev-setup.sh
+++ Linux/fai-config/files/home/dev-setup.sh
@@ -5,4 +5,5 @@
 sudo /usr/sbin/dhclient enp3s0 
 [ -e WaiveScreen.nfs ] || mkdir WaiveScreen.nfs
 
-/usr/bin/sshfs dev:/home/chris/code/WaiveScreen WaiveScreen.nfs -C
+/usr/bin/sshfs dev:/home/chris/code/WaiveScreen WaiveScreen.nfs -C -o allow_root
+
