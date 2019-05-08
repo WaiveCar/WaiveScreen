bcbf48b1d73f4771cbda33b494bbd0bc54c150b4
Chris McKenzie <kristopolous@yahoo.com>
2019-05-07 18:04:59 -0700

    Updating the dev script to work

--- Linux/fai-config/files/home/dev-setup.sh
+++ Linux/fai-config/files/home/dev-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-/usr/sbin/dhclient enp3s0 
+#
+# note! runs as normal user
+#
+sudo /usr/sbin/dhclient enp3s0 
 [ -e WaiveScreen.nfs ] || mkdir WaiveScreen.nfs
+
 /usr/bin/sshfs chris@172.16.10.1:/home/chris/code/WaiveScreen WaiveScreen.nfs -C
