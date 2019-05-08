44d81a372d9c29514fc3ef75915db0fb2523e49c
Chris McKenzie <kristopolous@yahoo.com>
2019-05-07 17:23:56 -0700

    Adding a line to sshfs mount the dev machine to keep things in sync

--- Linux/fai-config/files/home/dev-setup.sh
+++ Linux/fai-config/files/home/dev-setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/sbin/dhclient enp3s0 &
+/usr/sbin/dhclient enp3s0 
+[ -e WaiveScreen.nfs ] || mkdir WaiveScreen.nfs
+/usr/bin/sshfs chris@172.16.10.1:/home/chris/code/WaiveScreen WaiveScreen.nfs -C
