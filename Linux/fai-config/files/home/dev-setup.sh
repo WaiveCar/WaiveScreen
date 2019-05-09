6558b4046f97bddc9550cb4668e9c38cd68e9bc0
Chris McKenzie <kristopolous@yahoo.com>
2019-05-09 15:18:30 -0700

    refactoring dev-setup to the lib

--- Linux/fai-config/files/home/dev-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# note! runs as normal user
-#
-sudo /usr/sbin/dhclient enp3s0 
-[ -e WaiveScreen.nfs ] || mkdir WaiveScreen.nfs
-
-/usr/bin/sshfs dev:/home/chris/code/WaiveScreen WaiveScreen.nfs -C -o allow_root
-
