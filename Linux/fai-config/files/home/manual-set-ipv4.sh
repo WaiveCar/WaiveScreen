9f0559e71183c470a8977f71d14c24ddddd70828
Chris McKenzie <kristopolous@yahoo.com>
2019-05-09 15:14:26 -0700

    refactoring the manual ip setup

--- Linux/fai-config/files/home/manual-set-ipv4.sh
+++ Linux/fai-config/files/home/manual-set-ipv4.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 
-set -x
-export PATH=$PATH:/usr/bin/
+. lib.sh
 
-mmcli -m 0 -e
-
-mmcli -m 0 \
-	--location-enable-gps-raw \
-	--location-enable-gps-nmea \
-	--location-set-enable-signal
-
-mmcli -m 0 --simple-connect="apn=internet"
-wwan=`ip addr show | grep wwp | head -1 | awk -F ':' ' { print $2 } '`
-
-if [ -z "$wwan" ]; then
-  echo "Modem Not Found!!"
-  exit -1
-fi
-
-# get ipv6
-dhclient $wwan &
-
-# Show the config | find ipv4 | drop the LHS | replace the colons with equals | drop the whitespace | put everything on one line
-eval `mmcli -b 0 | grep -A 3 IPv4| awk -F '|' ' { print $2 } ' | sed s'/: /=/' | sed -E s'/\s+//' | tr '\n' ';'`
-
-ip addr add $address/$prefix  dev $wwan
-ip route add default via $gateway dev $wwan
-
-cat << ENDL | tee /etc/resolv.conf
-nameserver 8.8.8.8
-nameserver 4.2.2.1
-nameserver 2001:4860:4860::8888 
-nameserver 2001:4860:4860::8844
-ENDL
-
+modem_enable
+modem_connect
