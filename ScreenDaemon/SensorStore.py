b30bb412a9e2931041102a74329985aef95fe115
Chris McKenzie <kristopolous@yahoo.com>
2019-05-09 14:58:27 -0700

    naming consistencies for the sensor store and making it so that it works without the modem working

--- ScreenDaemon/SensorStore.py
+++ ScreenDaemon/SensorStore.py
@@ -6,6 +6,7 @@ import sys
 import dbus
 import time
 import pprint
+from datetime import datetime
 
 bus = dbus.SystemBus()
 
@@ -20,18 +21,19 @@ def next_iface():
     'time': dbus.Interface(proxy, dbus_interface='org.freedesktop.ModemManager1.Modem.Time')
   }
   modem_ix += 1 
-  if modem_ix > 10:
-      sys.exit(-1)
 
   return iface
 
 iface = next_iface()
 
-while not foundModem:
+# Try 10 eps to find a modem.
+for i in range(0, 10):
   try: 
     iface['location'].GetLocation()
-    #location = iface['time'].GetNetworkTime()
+
+    # if we get here then we know that our modem works
     foundModem = True
+    break
 
   except Exception as inst:
     print(inst)
@@ -41,18 +43,28 @@ while not foundModem:
 arduino.setup()
 while True:
   sensor = arduino.arduino_read()
-  modem = iface['location'].GetLocation()
-  nettime = iface['time'].GetNetworkTime()
+
+  # Put data in if we have it
+  location = {}
+  net_time = 0
+  system_time = datetime.now().replace(microsecond=0).isoformat()
+
+  if foundModem:
+    modem = iface['location'].GetLocation()
+    net_time = iface['time'].GetNetworkTime()
   
-  try:
+    try:
       location = {
         'lat': "{:.5f}".format(modem[2]['latitude']),
         'lng': "{:.5f}".format(modem[2]['longitude'])
       }
-  except:
-      location = {}
+    except:
+      pass
 
-  all = {**location, **sensor, 'time': nettime } 
+  # We can xref the net_time and system_time for now. Eventually this will
+  # probably not be necessary but in the early stages (2019/05/09) this is
+  # a sanity check.
+  all = {**location, **sensor, 'GPStime': net_time, 'SystemTime': system_time } 
   pprint.pprint(all)
   time.sleep(5)
 
