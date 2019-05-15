a1f7e4d89052bce1834b400e6cac5341daf79f53
Chris McKenzie <kristopolous@yahoo.com>
2019-05-15 15:44:09 -0700

    Adding a monitor to look at the file syncer before a deploy

--- tools/syncer.sh
+++ tools/syncer.sh
@@ -7,5 +7,6 @@ while [ 0 ]; do
   sudo cp -pru $CODE/* /srv/fai/config
   sudo chown -R root.root /srv/fai/config/scripts
   ssh screen "./dcall sync_scripts"
+  date +%X > /tmp/last-sync
   inotifywait -qe close_write,attrib,modify,move,move_self,create,delete,delete_self -r $CODE
 done
