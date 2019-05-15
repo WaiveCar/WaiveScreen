249ca254a506299c657d3eb1963e8fcc6afe0c8b
Chris McKenzie <kristopolous@yahoo.com>
2019-05-15 16:53:46 -0700

    Adding an ability to show the last sync time for deploys

--- tools/syncer.sh
+++ tools/syncer.sh
@@ -7,6 +7,7 @@ while [ 0 ]; do
   sudo cp -pru $CODE/* /srv/fai/config
   sudo chown -R root.root /srv/fai/config/scripts
   ssh screen "./dcall sync_scripts"
-  date +%X > /tmp/last-sync
+  date +%s > /tmp/last-sync
+  echo -n "."
   inotifywait -qe close_write,attrib,modify,move,move_self,create,delete,delete_self -r $CODE
 done
