5162a9cd78cc0ec81c5cfe5b9556346f2c24efac
Chris McKenzie <kristopolous@yahoo.com>
2019-05-27 21:20:55 -0700

    Updating the syncer to have a fail case

--- tools/syncer.sh
+++ tools/syncer.sh
@@ -6,9 +6,14 @@ CODE=$DIR/../Linux/fai-config
 while [ 0 ]; do
   sudo cp -pru $CODE/* /srv/fai/config
   sudo chown -R root.root /srv/fai/config/scripts
-  ssh screen "./dcall sync_scripts"
 
-  date +%X | osd_cat \
+  if ssh screen "./dcall sync_scripts"; then
+    fn=$(date +%X)
+  else
+    fn="!! Failure !!"
+  fi
+  
+  echo $fn| osd_cat \
       -c white \
       -p top -A right \
       -l 1 \
@@ -17,6 +22,5 @@ while [ 0 ]; do
       -f lucidasanstypewriter-bold-14 &
 
   date +%s > /tmp/last-sync
-  echo -n "."
   inotifywait -qe close_write,attrib,modify,move,move_self,create,delete,delete_self -r $CODE
 done
