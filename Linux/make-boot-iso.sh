67d52e4ccbbc37c5fb9371ff8bff471b7b66a492
Chris McKenzie <kristopolous@yahoo.com>
2019-05-28 18:23:43 -0700

    Adding a way to skip mirroring in the boot iso creation

--- Linux/make-boot-iso.sh
+++ Linux/make-boot-iso.sh
@@ -3,9 +3,14 @@
 dir=${1:-$HOME/usb}
 file=${2:-bootable.iso}
 
-echo "Using $dir - some serious space is probably needed"
-mkdir -p $dir
-fai-mirror -v -cDEBIAN $dir
-fai-cd -m $dir $file
+if [ "$NOMIRROR" ]; then
+  echo "Skipping mirroring"
+else
+  echo "Using $dir - some serious space is probably needed"
+  mkdir -p $dir
+  fai-mirror -v -cDEBIAN $dir
+fi
+
+sudo fai-cd -m $dir $file
 echo "Created a bootable iso named $file"
 
