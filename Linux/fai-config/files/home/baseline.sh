8fe2676b4e2d3635dafede371a3f16a1d83caf10
Chris McKenzie <kristopolous@yahoo.com>
2019-05-29 16:48:25 -0700

    Adding a warning for a bad mount in the baseline

--- Linux/fai-config/files/home/baseline.sh
+++ Linux/fai-config/files/home/baseline.sh
@@ -3,8 +3,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/const.sh
 
-
 sync_scripts() {
-  rsync --exclude=.xinitrc -aqzr $DEV/Linux/fai-config/files/home/ $DEST
-  chmod 0600 $DEST/.ssh/KeyBounce $DEST/.ssh/github $DEST/.ssh/dev
+  source=$DEV/Linux/fai-config/files/home/
+
+  if [ ! -e $source ]; then
+    echo "Warning: $source doesn't exist."
+  else
+    rsync --exclude=.xinitrc -aqzr $source $DEST
+    chmod 0600 $DEST/.ssh/KeyBounce $DEST/.ssh/github $DEST/.ssh/dev
+  fi
 }
