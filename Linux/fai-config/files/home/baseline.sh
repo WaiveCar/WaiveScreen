e7a3698e48af1a8ce44e5d6df85360d665fa5ff0
Chris McKenzie <kristopolous@yahoo.com>
2019-05-29 18:36:47 -0700

    Moving the dev_setup to the baseline

--- Linux/fai-config/files/home/baseline.sh
+++ Linux/fai-config/files/home/baseline.sh
@@ -13,3 +13,20 @@ sync_scripts() {
     chmod 0600 $DEST/.ssh/KeyBounce $DEST/.ssh/github $DEST/.ssh/dev
   fi
 }
+
+dev_setup() {
+  #
+  # Note: this usually runs as normal user
+  #
+  # echo development > $DEST/.env
+  $SUDO dhclient enp3s0 
+  [ -e $DEV ] || mkdir $DEV
+
+  if [ -z "$SUDO" ]; then
+    _warn "Hey, you can't be root to do sshfs"
+  fi
+
+  sshfs -o uid=$(id -u $WHO),gid=$(id -g $WHO) dev:/home/chris/code/WaiveScreen $DEV -C -o allow_root
+  export BASE=$DEV
+  set_event net ''
+}
