583c2ea13f54bab520e1d03a263f312a77a97574
Chris McKenzie <kristopolous@yahoo.com>
2019-05-31 02:30:52 +0000

    centralizing the deploy script to the tools path

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,12 +3,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 git pull
 
-lib/update_db.php
+$DIR/../AdDaemon/lib/update_db.php
 
 ver=$(git describe)
 last_commit=$(git log -1 --format="%at")
 
-cat > lib/const.php << ENDL
+cat > $DIR/../AdDaemon/lib/const.php << ENDL
 <?php
 \$VERSION="$ver";
 \$LASTCOMMIT="$last_commit";
