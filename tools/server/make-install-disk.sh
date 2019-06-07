700a0c8773fac763ea88d0e8d96aeca9855f53db
Chris McKenzie <kristopolous@yahoo.com>
2019-06-07 06:52:55 -0700

    fixing the broken boot creation

--- tools/server/make-install-disk.sh
+++ tools/server/make-install-disk.sh
@@ -3,6 +3,7 @@
 # NODISK    - Won't do disk things 
 # NONET     - Won't try to do internet stuff
 # NOMIRROR  - Won't do the mirroring
+# ONLYDISK  - Only creates a disk
 #
 ##
 
@@ -22,10 +23,12 @@ ddcmd() {
   echo "dd if=$1 | pv -s $size | sudo dd of=$2 bs=2M"
 }
 
-if [ -z "$NODIllSK" ]; then
+if [ -z "$NODISK" ]; then
   [ $# -lt 1 ] && die "You need to pass a disk dev entry to install to such as /dev/sdb"
   disk=$1
   [ -b $disk ] || die "Woops, $disk isn't a disk"
+  [ $(stat -c %T $disk) -eq 11 ] && die "Woah, $disk is a PARTITION. I need the whole disk."
+
 
   echo "Installing to $disk"
   sudo fdisk -l $disk
@@ -48,7 +51,7 @@ if [ "$NOMIRROR" ]; then
   echo "Skipping mirroring"
 else
   echo "Using $dir - some serious space is probably needed"
-  [ -e $dir ] && rm -rf $dir
+  # [ -e $dir ] && rm -rf $dir
   mkdir -p $dir
   fai-mirror -v -cDEBIAN $dir
 fi
