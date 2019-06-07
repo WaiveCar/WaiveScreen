bef9a3ab137c59739d83b047ea90ab7b5660355f
Chris McKenzie <kristopolous@yahoo.com>
2019-06-07 05:34:49 -0700

    trying to avoid a common fai bug

--- tools/server/make-install-disk.sh
+++ tools/server/make-install-disk.sh
@@ -10,17 +10,27 @@ die() {
   echo $1
   exit 1
 }
+
+ddcmd() {
+  size=$(stat -c %s $1)
+  echo "dd if=$1 | pv -s $size | sudo dd of=$2 bs=2M"
+}
+
 if [ -z "$NODISK" ]; then
   [ $# -lt 1 ] && die "You need to pass a disk dev entry to install to such as /dev/sdb"
   disk=$1
   echo "Installing to $disk"
   sudo fdisk -l $disk
+  if [ -n "$ONLYDISK" ]; then
+    $(ddcmd $(ls -tr1 Wai*| tail -1) $disk)
+  fi
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 dir=$HOME/usb
 file=$HOME/WaiveScreen-$(date +%Y%m%d%H%M)-$(git describe).iso
+backup=/home/chris/backup-test
 
 die() {
   echo $1
@@ -44,22 +54,33 @@ fi
 
 echo "Checking for the most common fai fail case"
 for i in aptcache  conf  db  dists  pool; do
-  [ -e $dir/$i ] || die "Nope nope nope. fai-cd will fail without $dir/$i existing"
+  if [ ! -e $dir/$i ]; then
+    echo "Nope nope nope. fai-cd will fail without $dir/$i existing"
+    if [ -e $backup/$i ]; then
+      echo "Trying to be brilliant and copy over a backup - don't ask me, I only work here."
+      cp -r $backup/$i $dir
+    else
+      die "Can't find a backup either. Fuck all this."
+    fi
+  else 
+    echo "Making a new backup of $i for future fai failures!"
+    [ -e $backup/$i ] && rm -fr $backup/$i
+    cp -r $dir/$i $backup
+  fi
 done
 
 echo "Creating a bootable iso named $file"
 sudo fai-cd -m $dir $file
-size=$(stat -c %s $file)
 
 if [ -z "$NODISK" ]; then
 
   if [ -e "$file" ]; then
     echo "Writing to $disk"
-    dd if=$file | pv -s $size | sudo dd of=$disk bs=2M
+    $(ddcmd $file $disk)
   else
     echo "$file does not exist, Bailing!"
   fi
 else 
-  echo dd if=$file | pv -s $size | sudo dd of=$disk bs=2M
+  echo $(ddcmd $file $disk)
 fi
 
