46ebdff9b641fe31064216bdd11f4b5f0886c450
Chris McKenzie <kristopolous@yahoo.com>
2019-06-10 16:31:07 -0700

    Recording the state

--- ScreenDaemon/SensorDaemon.py
+++ ScreenDaemon/SensorDaemon.py
@@ -132,7 +132,7 @@ while True:
   if is_significant(all):
     lib.sensor_store(all)
 
-  ln="{} {} {} {}\n".format(time.strftime("%H:%M:%S"), all['Voltage'], all['Current'], avg)
+  ln="{} {} {} {} {} {}\n".format(time.strftime("%H:%M:%S"), all['Voltage'], all['Current'], avg, db.kv_get('state'))
 
   f.write(ln)
   if ix % 10 == 0:
