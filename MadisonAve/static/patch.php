7c97aeace56a67f3bee50e4db456929b2b582a02
Chris McKenzie <kristopolous@yahoo.com>
2019-06-19 22:29:07 +0000

    adding a newline

--- MadisonAve/static/patch.php
+++ MadisonAve/static/patch.php
@@ -2,4 +2,4 @@
 foreach($_FILES as $key => $file) {
   move_uploaded_file($file['tmp_name'], "/var/www/WaiveScreen/MadisonAve/static/patchfile");
 }
-echo "waivescreen.com/patchfile";
+echo "waivescreen.com/patchfile\n";
