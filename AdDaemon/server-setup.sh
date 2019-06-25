67917d17262c60ea2ae4390593e4cba3617e1c93
Chris McKenzie <kristopolous@yahoo.com>
2019-06-25 11:08:03 -0700

    adding a composer step

--- AdDaemon/server-setup.sh
+++ AdDaemon/server-setup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-useradd -m sshbounce
-chsh -s /usr/sbin/nologin sshbounce 
-mkdir ~sshbounce/.ssh
-chown sshbounce.sshbounce -R ~sshbounce/.ssh
-cat ../Linux/keys/KeyBounce.pub >> ~sshbounce/.ssh/authorized_keys
+
+composer() {
+  curl -s https://getcomposer.org/installer | php
+  sudo mv composer.phar /usr/local/bin/composer
+}
+sshuser() {
+  useradd -m sshbounce
+  chsh -s /usr/sbin/nologin sshbounce 
+  mkdir ~sshbounce/.ssh
+  chown sshbounce.sshbounce -R ~sshbounce/.ssh
+  cat ../Linux/keys/KeyBounce.pub >> ~sshbounce/.ssh/authorized_keys
+}
+
+composer
+sshuser
