3d5673575f221e54cb9ed6bb3e9078f114d35e8e
Chris McKenzie <kristopolous@yahoo.com>
2019-06-27 15:13:45 -0700

    adding a basic screen interface

---
+++ Admin/lib.php
@@ -0,0 +1,15 @@
+<?
+
+function get($ep) {
+  return json_decode(file_get_contents("http://waivescreen.com/api/$ep"), true);
+}
+function get_addressList($list) {
+  $url="http://basic.waivecar.com/location.php?multi=" . urlencode(json_encode($list));
+  return curldo($url);
+}
+
+function get_address($obj) {
+  $url="http://basic.waivecar.com/location.php?latitude=${obj['lat']}&longitude=${obj['lng']}";
+  return curldo($url, ['raw' => true]);
+}
+
--- Admin/screens.php
+++ Admin/screens.php
@@ -2,15 +2,32 @@
 include('../MadisonAve/lib/lib.php');
 include('lib.php');
 
-$campaignList = get('campaigns');
+$screenList = get('screens');
 $addrList = get_addressList(array_map(function($row) { 
-  return [$row['lat'],$row['lng']]; 
-}, $campaignList));
+  if($row['lat'] && $row['lng']) {
+    return [$row['lat'],$row['lng']]; 
+  } else  {
+    return [];
+  }
+}, $screenList));
 
-for($ix = 0; $ix < count($campaignList); $ix++){
-  $campaignList[$ix]['addr'] = $addrList[$ix];
+for($ix = 0; $ix < count($screenList); $ix++){
+  if($addrList[$ix]) {
+    $screenList[$ix]['addr'] = "<a href=//maps.google.com/?q={$screenList[ix]['lat']},{$screenList[ix]['lng']}>{$addrList[$ix]}</a>";
+  } else {
+    $screenList[$ix]['addr'] = 'unknown';
+  }
+  $screenList[$ix]['last_local'] = time() - strtotime(str_replace(' ', 'T', $screenList[$ix]['last_seen'] . 'Z'));
 }
 
+$fieldList = [
+  'id'  => 'uid',
+  'car' => 'car',
+  'location' => 'addr',
+  'phone' => 'phone',
+  'version' => 'version',
+  'last' => 'last_local'
+];
 ?>
 <!doctype html>
 <html lang="en">
@@ -24,35 +41,30 @@ for($ix = 0; $ix < count($campaignList); $ix++){
 
     <title>Admin panel</title>
   </head>
+  <style>
+    .id,.version { font-family: monospace}
+  </style>
   <body>
-   <h2> Campaigns</h2>
-    <div class='row'>
-    <? foreach($campaignList as $campaign) { 
-      $done = min($campaign['completed_seconds'] / $campaign['duration_seconds'], 1) * 100;
-?>
-      <div class="card" style="width: 18rem;">
-        <img src="<?= $campaign['asset'][0] ?>" class="card-img-top">
-        <div class="card-body">
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: <?= $done ?>%" aria-valuenow="<?= $done ?>" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-          <p><?= $campaign['completed_seconds'] ?>/<?= $campaign['duration_seconds'] ?>s complete<br/>
-          <a href="https://maps.google.com/?q=<?= $campaign['lat'] ?>,<?= $campaign['lng'] ?>"><?= $campaign['addr']; ?></a><br/>
-          Radius: <?= $campaign['radius'] ?>m</p>
-
-          Start: <?= $campaign['start_time'] ?><br>
-          End: <?= $campaign['end_time'] ?>
-
-
-user
-  
-          <p class="card-text">user</p>
-          <a href="#" class="btn btn-primary">Disable</a>
-        </div>
-      </div>
+   <h2>screens</h2>
+  <table class="table">
+    <thead>
+      <tr>
+      <? foreach($fieldList as $key => $value) { ?>
+        <th scope="col"><?= $key ?></th>
+      <? } ?>
+      </tr>
+    </thead>
+  <tbody>
+    <? foreach($screenList as $screen) { ?>
+      <tr>
+        <? foreach($fieldList as $name => $key) { ?>
+          <td class="<?= $name?>"><?= $screen[$key] ?></td>
+      <? } ?>
+      </tr>
     <? } ?>
+  </tbody>
+  </table>
     </div>
-    Users
     <script src="engine.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
