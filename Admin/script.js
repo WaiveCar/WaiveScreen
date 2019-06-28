555cd049c08aa015d034a254c7c3ebd01fce26d2
Chris McKenzie <kristopolous@yahoo.com>
2019-06-27 18:22:36 -0700

    Getting the edit to work

--- Admin/script.js
+++ Admin/script.js
@@ -1,11 +1,17 @@
-function change(id, what) {
-  var newval = prompt(`Change the ${what}`)
-  if(newval) {
+function change(id, what, el) {
+  let dom = el.parentNode.firstElementChild;
+  var newval = prompt(`Change the ${what}`, dom.innerHTML)
+  if(newval !== null) {
+    dom.innerHTML = '&#8987;...';
     fetch(new Request('/api/screens', {
       method: 'POST', 
       body: JSON.stringify({id: id, [what]: newval})
     })).then(res => {
-      console.log('success', res);
+      if (res.status === 200) {
+        return res.json();
+      }
+    }).then(res => {
+      dom.innerHTML = res[what];
     });
   }
 }
