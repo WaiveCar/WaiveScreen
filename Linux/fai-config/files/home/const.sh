f7c5853eb67dc2a3b60af679054c74dfa1932110
Chris McKenzie <kristopolous@yahoo.com>
2019-06-28 16:40:42 -0700

    Adding cache to the const

--- Linux/fai-config/files/home/const.sh
+++ Linux/fai-config/files/home/const.sh
@@ -10,6 +10,7 @@ export DISPLAY=${DISPLAY:-:0}
 export VID=/var/capture
 export LOG=/var/log/screen
 export DB=/var/db/config.db
+export CACHE=/var/cache/assets
 export ENV=production
 
 [[ $USER = 'root' ]] && SUDO= || SUDO=/usr/bin/sudo
