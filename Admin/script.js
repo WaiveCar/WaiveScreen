f376a43bff63fd07ccbc8f3cfd4331ef9c004228
Chris McKenzie <kristopolous@yahoo.com>
2019-06-27 17:57:48 -0700

    Working on a facility to rename the car

--- Admin/script.js
+++ Admin/script.js
@@ -1,4 +1,12 @@
 function change(id, what) {
-  var newval = prompt(`Change the $what`)
+  var newval = prompt(`Change the ${what}`)
+  if(newval) {
+    fetch(new Request('/api/screens', {
+      method: 'POST', 
+      body: JSON.stringify({id: id, [what]: newval})
+    })).then(res => {
+      console.log('success', res);
+    });
+  }
 }
 
