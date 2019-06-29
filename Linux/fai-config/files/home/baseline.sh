f30d0c7e6bf5fb44b02b0942fb013489ee978b03
Chris McKenzie <kristopolous@yahoo.com>
2019-06-28 18:30:08 -0700

    trying to add SHA1 support to local_sync for rollback #40

--- Linux/fai-config/files/home/baseline.sh
+++ Linux/fai-config/files/home/baseline.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sync_scripts() {
   local source=${1:-$DEV/Linux/fai-config/files/home/}
 
-  if [ ! -e $source ]; then
+  if [[ ! -e $source ]]; then
     echo "Warning: $source doesn't exist."
   else
     rsync --exclude=.xinitrc,locals.sh -aqzr $source $DEST
@@ -16,22 +16,19 @@ sync_scripts() {
 }
 
 dev_setup() {
-  #
-  # Note: this usually runs as normal user
-  #
   $SUDO pkill dhclient
   oldroute=$(ip route show | grep default | awk ' { print $1" "$2" "$3 }')
-  [ -n "$oldroute" ] && $SUDO ip route delete $oldroute
+  [[ -n "$oldroute" ]] && $SUDO ip route delete $oldroute
 
   $SUDO dhclient enp3s0 
-  [ -e $DEV ] || mkdir $DEV
+  [[ -e $DEV ]] || mkdir $DEV
 
   _as_user sshfs -o uid=$(id -u $WHO),gid=$(id -g $WHO),nonempty,allow_root dev:/home/chris/code/WaiveScreen $DEV -C 
   export BASE=$DEV
 }
 
 _as_user() {
-  [ $USER = 'root' ] && su $WHO -c "$*" || eval $*
+  [[ $USER = 'root' ]] && su $WHO -c "$*" || eval $*
 }
 
 _git() {
@@ -42,11 +39,13 @@ local_sync() {
   # Since everything is in memory and already loaded
   # we can try to just pull things down
   cd $BASE
+  local sha1=${1:-master}
   
   # We make sure that local changes (there shouldn't be any)
   # get tossed aside and pull down the new code.
   _git stash
   if _git pull; then
+    _get checkout $sha1
 
     # If there's script updates we try to pull those down
     # as well - we can use our pre-existing sync script
