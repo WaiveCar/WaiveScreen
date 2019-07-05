1d4a1ef794c3fa06d1a3fd580187e6e539cb3fbb
Chris McKenzie <kristopolous@yahoo.com>
2019-07-04 20:08:56 -0700

    Fixing the UUID display

--- ScreenDisplay/display.js
+++ ScreenDisplay/display.js
@@ -3,7 +3,7 @@ window.onload = function init() {
     server: 'http://localhost:4096/'
   });
   ads.on('system', function(data) {
-    document.getElementsByClassName('info')[0].innerHTML = [data.number.slice(-7), data.uuid.slice(5)].join(' ');
+    document.getElementsByClassName('info')[0].innerHTML = [data.number.slice(-7), data.uuid.slice(0,5)].join(' ');
   });
   ads.Start();
 }
