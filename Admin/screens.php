3fb9ad773ca266e0f55f2d7df9177c710d39156c
Chris McKenzie <kristopolous@yahoo.com>
2019-07-04 17:31:28 -0700

    a bunch of crap for sbadmin

--- Admin/screens.php
+++ Admin/screens.php
@@ -55,6 +55,9 @@ $editable = ['car', 'phone'];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel=stylesheet href=https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
     <title>Admin panel</title>
   </head>
   <style>
@@ -65,9 +68,17 @@ $editable = ['car', 'phone'];
     em { color: #555 }
     .edit:hover { color: #000 }
   </style>
-  <body>
+  <body id="page-top">
+  <div id="wrapper">
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    </ul>
+  <div id="content-wrapper" class="d-flex flex-column">
+
   <h2>screens <span class='id'><?= $VERSION?></span></h2>
-  <table class="table">
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
     <thead>
       <tr>
       <? foreach($fieldList as $key => $value) { ?>
@@ -94,10 +105,16 @@ $editable = ['car', 'phone'];
     <? } ?>
   </tbody>
   </table>
-    </div>
+ </div>
+</div>
     <script src="/Admin/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="/js/jquery.easing.min.js"></script>
+    <script src="/js/sb-admin-2.min.js"></script>
+    <script src="/js/jquery.dataTables.min.js"></script>
+    <script src="/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/js/datatables-demo.js"></script>
   </body>
 </html>
