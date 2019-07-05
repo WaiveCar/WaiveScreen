d58bfcf639f9a855ea85f37d2293f824d7e2dc42
Chris McKenzie <kristopolous@yahoo.com>
2019-07-04 19:09:46 -0700

    Implementing a basic event system for displaying data on screen

--- ScreenDisplay/display.js
+++ ScreenDisplay/display.js
@@ -2,5 +2,8 @@ window.onload = function init() {
   self.ads = Engine({
     server: 'http://localhost:4096/'
   });
+  ads.on('system', function(data) {
+    document.getElementsByClassName('info')[0].innerHTML = [data.number.slice(-7), data.uuid.slice(5)].join(' ');
+  });
   ads.Start();
 }
