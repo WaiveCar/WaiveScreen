f344e777dc9f184e2087ba3b31dc34718b000b3c
Chris McKenzie <kristopolous@yahoo.com>
2019-07-12 15:48:25 -0700

    updating the pycall for the autobright

--- Linux/fai-config/files/home/start-x-stuff.sh
+++ Linux/fai-config/files/home/start-x-stuff.sh
@@ -35,7 +35,7 @@ disk_monitor
 # but we are putting it in here to not
 # cause any problems
 #
-NOMODEM=1 pycall set_autobright
+NOMODEM=1 pycall arduino.set_autobright
 
 #
 # This delay is rather important because the
