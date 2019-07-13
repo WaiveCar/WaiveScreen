77b8c561e5b8ad1dcb6b3d1751e9d727c40cb450
Chris McKenzie <kristopolous@yahoo.com>
2019-07-13 15:02:20 -0700

    enabling branch selection in the overrides

--- Linux/fai-config/files/home/const.sh
+++ Linux/fai-config/files/home/const.sh
@@ -2,18 +2,21 @@
 
 export WHO=adorno
 export DEST=/home/$WHO
-export PATH=/usr/bin:/usr/sbin:$PATH:$DEST
 export BASE=$DEST/WaiveScreen
-export ROOTHOME=/root
-export DEV=$BASE.sshfs
-export EV=/tmp/event
-export DISPLAY=${DISPLAY:-:0}
-export VID=/var/capture
-export LOG=/var/log/screen
-export DB=/var/db/config.db
+
+export BRANCH=master
 export CACHE=/var/cache/assets
+export DB=/var/db/config.db
+export DEV=$BASE.sshfs
+export DISPLAY=${DISPLAY:-:0}
 export ENV=production
 export EVREST=20
+export EV=/tmp/event
+export LOG=/var/log/screen
+export PATH=/usr/bin:/usr/sbin:$PATH:$DEST
+export ROOTHOME=/root
+export VID=/var/capture
+
 [[ $USER != 'root' ]] && SUDO=/usr/bin/sudo
 
 # Local overrides to the above 
