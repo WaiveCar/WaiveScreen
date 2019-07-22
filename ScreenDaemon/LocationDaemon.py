2618acbe8cc4643f09fe2e648836519d9f338260
Nick Thobe <nthobe@waive.car>
2019-07-22 14:43:22 -0700

    Comments, min_bss_count, enabled loop, 10 minute wifi interval

--- ScreenDaemon/LocationDaemon.py
+++ ScreenDaemon/LocationDaemon.py
@@ -11,8 +11,11 @@ import lib.db as db
 
 logging.basicConfig(level=logging.DEBUG)
 
+# location_loop sleep time - applies to GPS polling
 SLEEP_TIME = 10
-MAX_WIFI_INTERVAL = 60 #0
+# extra time to wait if we're using the wifi location service
+# We don't want to spam MLS and hit their 100,000 daily request limit
+MIN_WIFI_INTERVAL = 600
 _current_location_source = None
 _last_wifi_location_request_time = 0.0
 
@@ -28,6 +31,7 @@ def get_location_from_gps():
   return l
 
 def save_location(location):
+  # Save current location info to the database
   logging.debug("Saving location: lat:{} lng:{} accuracy:{}".format(location['Lat'], location['Lng'], location.get('accuracy')))
   db.kv_set('lat', location['Lat'])
   db.kv_set('lng', location['Lng'])
@@ -35,6 +39,8 @@ def save_location(location):
   db.kv_set('location_source', _current_location_source)
 
 def location_loop():
+  # We prefer the location from the GPS.  If that fails,
+  # we try and determine our location based on a Wifi scan.
   global _current_location_source, _last_wifi_location_request_time
   while True:
     location = False
@@ -45,7 +51,7 @@ def location_loop():
           stop_wifi_location_service()
         _current_location_source = 'gps'
         save_location(location)
-      elif time.time() - _last_wifi_location_request_time >= MAX_WIFI_INTERVAL:
+      elif time.time() - _last_wifi_location_request_time >= MIN_WIFI_INTERVAL:
         location = get_location_from_wifi()
         _last_wifi_location_request_time = time.time()
         _current_location_source = 'wifi'
@@ -54,11 +60,11 @@ def location_loop():
         else:
           logging.info('Unable to determine our location.  GPS and Wifi failed.')
     except Exception as ex:
-      logging.error('Error in main loop: {}'.format(ex))
+      logging.error('Error in location_loop: {}'.format(ex))
 
     time.sleep(SLEEP_TIME)
 
 
-#if __name__ == '__main__':
-#  location_loop()
+if __name__ == '__main__':
+  location_loop()
 
