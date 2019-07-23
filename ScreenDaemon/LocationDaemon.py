ef472fec9f97996c40f336a6cf380d140e5c07ef
Nick Thobe <nthobe@waive.car>
2019-07-22 19:03:40 -0700

    moved wifi_last_submission and location_source to db.kv, fixed bug with not removing signals

--- ScreenDaemon/LocationDaemon.py
+++ ScreenDaemon/LocationDaemon.py
@@ -5,7 +5,7 @@
 
 import time
 import logging
-from lib.wifi_location import wifi_location, wifi_scan_shutdown
+from lib.wifi_location import wifi_location, wifi_scan_shutdown, wifi_last_submission
 from lib.lib import get_gps
 import lib.db as db
 
@@ -16,8 +16,6 @@ SLEEP_TIME = 10
 # extra time to wait if we're using the wifi location service
 # We don't want to spam MLS and hit their 100,000 daily request limit
 MIN_WIFI_INTERVAL = 600
-_current_location_source = None
-_last_wifi_location_request_time = 0.0
 
 def get_location_from_wifi():
   l = wifi_location()
@@ -36,28 +34,32 @@ def save_location(location):
   db.kv_set('lat', location['Lat'])
   db.kv_set('lng', location['Lng'])
   db.kv_set('location_accuracy', location.get('accuracy', ''))
-  db.kv_set('location_source', _current_location_source)
+
+def location_source(set_it=False):
+  if set_it:
+    db.kv_set('location_source', set_it)
+  else:
+    return db.kv_get('location_source')
 
 def location_loop():
   # We prefer the location from the GPS.  If that fails,
   # we try and determine our location based on a Wifi scan.
-  global _current_location_source, _last_wifi_location_request_time
   while True:
     location = False
     try:
       location = get_location_from_gps()
       if location:
-        if _current_location_source == 'wifi':
+        if location_source() != 'gps':
           stop_wifi_location_service()
-        _current_location_source = 'gps'
+        location_source('gps')
         save_location(location)
-      elif time.time() - _last_wifi_location_request_time >= MIN_WIFI_INTERVAL:
+      elif time.time() - wifi_last_submission() >= MIN_WIFI_INTERVAL:
         location = get_location_from_wifi()
-        _last_wifi_location_request_time = time.time()
-        _current_location_source = 'wifi'
         if location:
+          location_source('wifi')
           save_location(location)
         else:
+          location_source('none')
           logging.info('Unable to determine our location.  GPS and Wifi failed.')
     except Exception as ex:
       logging.error('Error in location_loop: {}'.format(ex))
