99720aec9b72c5cd89cd7bb67f089f30da6041ba
Chris McKenzie <kristopolous@yahoo.com>
2019-07-23 23:17:01 +0000

    getting the backup script to work

--- tools/server/server-backup.sh
+++ tools/server/server-backup.sh
@@ -4,6 +4,6 @@ dir=/var/backup
 backupfile=db-$(date +%Y%m%d).gz
 db=/var/db/waivescreen/main.db
 [ -e $dir ] || mkdir -p $dir
-sqlite3 $db .dump | gzip -c $dir/$backupfile
-scp -i backupKey $dir/$backupfile x0MfDySLQXmtuC6W9hA1xg@9ol.es:/raid/backup/waivescreen/$backupfile -p 5001
+sqlite3 $db .dump | gzip -c > $dir/$backupfile
+scp -P 5001 -i /root/backupKey -o StrictHostKeyChecking=accept-new $dir/$backupfile x0MfDySLQXmtuC6W9hA1xg@9ol.es:/raid/backup/waivescreen/
 find $dir -ctime +7 -name \*.gz -exec rm -f {} \;
