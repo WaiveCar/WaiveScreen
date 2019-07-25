75a66e53ec8c717ef40440fa42eb67a8ae69a46d
Chris McKenzie <kristopolous@yahoo.com>
2019-07-25 22:54:30 +0000

    screen page fixes

--- Admin/screens.php
+++ Admin/screens.php
@@ -120,6 +120,11 @@ function split($str) {
     .edit { color: #999; cursor: pointer }
     .last { text-align: right }
     em { color: #555 }
+    .modal-body span {
+      min-width: 5rem; 
+      display: inline-block;
+      vertical-align: top;
+    }
     .edit:hover { color: #000 }
     #notice { position: absolute; top:0; left:0; width: 100%; z-index: 100;display:none}
   </style>
@@ -211,14 +216,11 @@ function split($str) {
           </div>
 
           <div class="modal-body">
-            ( a few redundant things ) <br/>
-            ( feature list ) <br/>
-            ( tag list ) (editable)
           </div>
 
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Update</button>
+            <!--<button type="button" class="btn btn-primary">Update</button>-->
           </div>
         </div>
       </div>
@@ -228,7 +230,10 @@ function split($str) {
     <script>
     var Data=<?=json_encode($screenList);?>
     </script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="/js/jquery.easing.min.js"></script>
