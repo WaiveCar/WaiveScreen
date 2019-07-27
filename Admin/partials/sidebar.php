287010a879453e8cdc0a497c2b0dd0173cd88edf
Chris McKenzie <kristopolous@yahoo.com>
2019-07-27 16:24:21 -0700

    typo fixes

--- Admin/partials/sidebar.php
+++ Admin/partials/sidebar.php
@@ -1,6 +1,6 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center">
     <div class="sidebar-brand-text mx-3">waive</div>
   </a>
   <hr class="sidebar-divider d-none d-md-block">
