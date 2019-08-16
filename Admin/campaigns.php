b6259d8aa6f7971d8a61f487570590ee16e9565f
Chris McKenzie <kristopolous@yahoo.com>
2019-08-16 16:29:46 -0700

    Adding a way to clear the map

--- Admin/campaigns.php
+++ Admin/campaigns.php
@@ -52,7 +52,7 @@ $height = $width * 675 / 1920;
                   <div class="progress">
                     <div class="progress-bar" role="progressbar" style="width: <?= $done ?>%" aria-valuenow="<?= $done ?>" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <p>Loction<?= $campaign['completed_seconds'] ?>/<?= $campaign['duration_seconds'] ?>s complete<br/>
+                  <p><?= $campaign['completed_seconds'] ?>/<?= $campaign['duration_seconds'] ?>s complete<br/>
                   Start: <?= $campaign['start_time'] ?><br>
                   End: <?= $campaign['end_time'] ?>
                 <? } else { ?>
@@ -118,15 +118,16 @@ $height = $width * 675 / 1920;
           </div>
 
           <div class="modal-body">
-      <select id="type">
-        <option value="Circle">Circle</option>
-        <option value="Polygon">Geofence</option>
-      </select>
+            <select id="type" class="custom-select custom-select-lg">
+              <option value="Circle">Circle</option>
+              <option value="Polygon">Geofence</option>
+            </select>
             <div style='width:100%;height:20vw' id='map'></div>
           </div>
 
           <div class="modal-footer">
             <button type="button" class="btn btn-danger btn-sm mr-auto" data-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-outline-secondary btn-sm mr-auto" onclick=clearmap()>Clear</button>
             <button type="button" class="btn btn-secondary" onclick=geosave()>Update</button>
           </div>
         </div>
@@ -137,7 +138,7 @@ $height = $width * 675 / 1920;
     </script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="/engine.js"></script>
-    <script src="/map-new.js"></script>
+    <script src="/Admin/dist/map.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
