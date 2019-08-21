24cecab05241266eb8c90f3cdc05fe09c59e498d
Chris McKenzie <kristopolous@yahoo.com>
2019-08-21 16:18:20 -0700

    putting the campaign create js in the admin panel

--- Admin/campaigns.php
+++ Admin/campaigns.php
@@ -46,7 +46,7 @@ $height = $width * 675 / 1920;
           <div class="alert alert-primary" id="notice" role="alert"></div>
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Campaigns</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> New</a>
+            <a onclick="create_campaign()" href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> New</a>
           </div>
           <div class='row'>
           <? foreach($campaignList as $campaign) { 
