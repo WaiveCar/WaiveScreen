17b5326722f57a4e89f10512bb9e3ab2a2dd72a6
Chris McKenzie <kristopolous@yahoo.com>
2019-08-26 17:29:29 -0700

    adding a tool to make the keyboard override disk

---
+++ tools/server/make-keyboard-override.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+die() {
+  exit $1
+  exit
+}
+
+[[ $# -lt 1 ]] && die "You need to pass a dev entry for it such as /dev/sdb1"
+
+disk=$1
+mount=/tmp/mount
+magic_override_file=voHCPtpJS9izQxt3QtaDAQ_make_keyboard_work
+
+[[ -e $disk ]] || die "Woops, $disk doesn't exist. Check your spelling."
+[[ -e $mount ]] || mkdir $mount
+sudo umount $mount >& /dev/null 
+sudo mount $disk $mount || die "Can't mount $disk on $mount - fix this and then rerun with the NOCLONE=1 env variable to skip the cloning"
+sudo touch $mount/$magic_override_file
+sudo umount $mount
