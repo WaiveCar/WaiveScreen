fed0914444b07a0acb6cd4c504945554b7b0fcaa
Chris McKenzie <kristopolous@yahoo.com>
2019-08-30 02:03:39 -0700

    preliminary support for websocket

--- ScreenDisplay/display.js
+++ ScreenDisplay/display.js
@@ -2,15 +2,15 @@ window.onload = function init() {
   self.ads = Engine({
     server: 'http://localhost:4096/'
   });
+
   ads.on('system', function(data) {
     document.getElementsByClassName('info')[0].innerHTML = [data.number.slice(-7), data.uuid.slice(0,5)].join(' ');
   });
 
   ads.Start();
 
-  ws.on("sms", function(sms) {
-  });
-
-  ws.on("showad", function(sms) {
+  var ws = new WebSocket("ws://127.0.0.1:4096/ws");
+  ws.onmessage = function(event) {
+    console.log(event.data);
   });
 }
