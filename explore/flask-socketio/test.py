b1508556f307f029ce7198e024bac1dc6ef46a04
Chris McKenzie <kristopolous@yahoo.com>
2019-09-07 23:53:28 -0700

    updating exploratory code

--- explore/flask-socketio/test.py
+++ explore/flask-socketio/test.py
@@ -1,17 +1,32 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import time
+from multiprocessing import Process, Queue
     
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+qData = Queue()
 
-@app.route('/')
+@app.route('/hi')
 def index():
-    return "";
+  #qData.put([1,2,3], block=False)
+  return ""
+
+@socketio.on('connect')
+def connect():
+  emit("connected", [1,2,3])
+
+@socketio.on('disconnect')
+def test_disconnect():
+  print('Client disconnected')
 
 @socketio.on('my event')
 def test_message(message):
-    emit('my response', {'data': 'got it!'})
+  emit('my response', [1,23])#, [a,b,c])
+  #a, b, c = qData.get()
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0')
+
