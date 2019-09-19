f5c5310b8339eac39bb023d211d1bf780487d482
David Alexander Leigh <daleighan@gmail.com>
2019-09-19 13:02:35 -0700

    Adding styles to currently selected link

--- reef-demo/menu.js
+++ reef-demo/menu.js
@@ -1,18 +1,27 @@
 (() => {
-  document.getElementById('menu')
-    .innerHTML = `
+  document.getElementById('menu').innerHTML = `
       <ul class="navbar-nav bg-gradient-white sidebar sidebar-light accordion" id="accordionSidebar">
         <a class="d-flex align-items-center justify-content-center">
           <div class="sidebar-brand-text mx-3" style=color:black>adcast</div>
         </a>
         <hr class="sidebar-divider d-none d-md-block">
-        ${['screens', 'campaigns', 'organizations', 'brands', 'users'].map(item => `
-          <li class="ml-2 nav-item menu-item sidebar-link active">
+        ${['screens', 'campaigns', 'organizations', 'brands', 'users']
+          .map(
+            item => `
+          <li class="ml-2 nav-item menu-item sidebar-link active" data-item="${item}">
             <a class="nav-link" href="/${item}/index.html">
-            <span>${item[0].toUpperCase() + item.slice(1)}</span></a>
+            <span>${item}</span></a>
           </li>
-        `).join('')}
+        `,
+          )
+          .join('')}
         <hr class="sidebar-divider d-none d-md-block">
       </ul>
-    `
+    `;
+  let items = document.querySelectorAll('.navbar-nav li');
+  items.forEach(one => {
+    if (window.location.href.includes(one.dataset.item)) {
+      one.querySelector('span').classList.add('current-link');
+    }
+  });
 })();
