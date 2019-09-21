9b028c1db54bbe0e1b8d2631ede5c9bcd88361a9
David Alexander Leigh <daleighan@gmail.com>
2019-09-20 17:34:31 -0700

    Added colors to pie charts

--- reef-demo/campaigns/show/charts.js
+++ reef-demo/campaigns/show/charts.js
@@ -1,6 +1,12 @@
 (() => {
   window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
     yellow: '#fedc47',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)',
   };
   chart1();
   chart2();
