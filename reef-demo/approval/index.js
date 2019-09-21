f3b2b5ea06191ec93ce9b72a9babd4f319272e70
David Alexander Leigh <daleighan@gmail.com>
2019-09-21 15:06:42 -0700

    Added additional logic for the approval page

--- reef-demo/approval/index.js
+++ reef-demo/approval/index.js
@@ -1,10 +1,14 @@
-function renderCampaigns(campaigns) {
+function renderCampaigns(campaigns, statusId) {
   moment.locale('en');
-  document.querySelectorAll('.campaign-list').forEach(node => node.innerHTML = campaigns
-    .map(
-      campaign =>
-        `<div class="card mt-1 ml-2">
-           <a class="prevent-underline" href="/campaigns/show?id=${campaign.id}">
+  document.querySelectorAll(`#${statusId}`).forEach(
+    node =>
+      (node.innerHTML = campaigns
+        .map(
+          campaign =>
+            `<div class="card mt-1 ml-2">
+           <a class="prevent-underline" href="/campaigns/show?id=${
+             campaign.id
+           }">
              <div id="asset-container-1"> 
                <div>
                  <img src="http://waivecar-prod.s3.amazonaws.com/311a1ccb-c3d0-4c42-995d-5d3d38af0bf2.jpeg" class="campaign-image-preview">
@@ -12,7 +16,11 @@ function renderCampaigns(campaigns) {
              </div>
              <div class="campaign-title mt-1">${campaign.project}</div>
              <div class="campaign-dates">
-               ${`${moment(campaign.start_time).format('MMM D')}   `}<i class="fas fa-play arrow"></i> ${`   ${moment(campaign.end_time).format('MMM D')}`}
+               ${`${moment(campaign.start_time).format(
+                 'MMM D',
+               )}   `}<i class="fas fa-play arrow"></i> ${`   ${moment(
+              campaign.end_time,
+            ).format('MMM D')}`}
              </div>
              <div class="user-icon-holder">
                <img src="../svg/user-icon.svg" class="user-icon">
@@ -22,15 +30,18 @@ function renderCampaigns(campaigns) {
              </div>
            </a>
          </div>`,
-    )
-    .join(''));
+        )
+        .join('')),
+  );
 }
 
 (() => {
   fetch('http://waivescreen.com/api/campaigns')
     .then(response => response.json())
     .then(json => {
-      renderCampaigns(json);
+      ['pending-campaigns', 'active-campaigns', 'paused-campaigns'].forEach(
+        status => renderCampaigns(json, status),
+      );
     })
     .catch(e => console.log('error fetching screens', e));
 })();
