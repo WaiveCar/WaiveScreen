1d9c355f536c1fb5b54756af045fa2489b69fb03
David Alexander Leigh <daleighan@gmail.com>
2019-09-21 17:11:35 -0700

    Moving new logic from login into signup

---
+++ reef-demo/signup/index.js
@@ -0,0 +1,27 @@
+let form = document.querySelector('form');
+
+function signup() {
+  let data = new FormData(form);
+  let object = {};
+  data.forEach((value, key) => {
+    object[key] = value;
+  });
+  let json = JSON.stringify(object);
+  console.log(json);
+}
+
+(() => {
+  document.querySelector('.form-fields').innerHTML = ['email', 'password'].map(field => `
+    <div class="form-group">
+      <input 
+        name="${field}" 
+        type="${field}" 
+        class="form-control" 
+        id="${field}" 
+        placeholder="${`${field[0].toUpperCase() + field.slice(1)}`}" 
+        autocomplete="off"
+      >
+    </div>
+  
+  `).join('');
+})();
