01e8eab0af35b3f5b3723e78cba8df7e41480059
David Alexander Leigh <daleighan@gmail.com>
2019-09-23 17:29:20 -0700

    Parsing some of the data for the widgets

--- reef-demo/APIWidget/print_tickers.py
+++ reef-demo/APIWidget/print_tickers.py
@@ -5,19 +5,69 @@ from sports import get_MLB_scores, get_NFL_scores
 from stocks import get_daily_stock_movement
 from traffic import get_traffic_incidents
 from uselessfact import get_random_fact
-import geocoder
-import hashlib
-latlng = geocoder.ip('me').latlng
+import hashlib, json, geocoder
+latlng = geocoder.ip("me").latlng
 
 def fetch_info():
-    return {
-        'catfact': get_cat_fact(),
-        'forecast': get_current_weather(get_forecast()),
-        'news': get_news_stories(),
-        'mlb_scores': get_MLB_scores(),
-        'nfl scores': get_NFL_scores(2),
-        'stocks': get_daily_stock_movement('MSFT'),
-        'random fact': get_random_fact()
+    raw_json = {
+        "catfact": get_cat_fact(),
+        "forecast": get_current_weather(get_forecast()),
+        "news": get_news_stories(),
+        "mlb_scores": get_MLB_scores(),
+        "nfl_scores": get_NFL_scores(2),
+        "stocks": get_daily_stock_movement("MSFT"),
+        "random fact": get_random_fact(),
+        "traffic": get_traffic_incidents(latlng[0], latlng[1], 3)
+    }
+    with open("./widgetfiles/parsed_widget_data.json", "w+") as f:
+        json.dump(raw_json, f)
+
+    parsed = {}
+    parsed["catfact"] = {
+        "feed": [raw_json["catfact"]], 
+        "source": "https://cat-fact.herokuapp.com/facts",
+        "expiration": None
     }
 
-print('output: ', fetch_info())
+    forecast = raw_json["forecast"]
+    parsed["forecast"] = {
+        "feed": [{
+            "text": "Today's forcast: {} with an actual temperature of {} degrees and a wind speed of {} mph.".format(forecast["summary"], forecast["temp"], forecast["wind_speed"]),
+            "image": None
+        }], 
+        "source": "https://dark-sky.p.rapidapi.com/",
+        "expiration": None
+    }
+
+    parsed["news"] = {
+        "feed": list(map(lambda obj: {
+            "text": "{}: {}".format(obj["source_name"], obj["description"]),
+            "image": obj["image_url"]
+        }, raw_json["news"])),
+        "source": "https://newsapi.org/v2/top-headlines",
+        "expiration": None
+    }
+
+    parsed["mlb_scores"] = {
+        "feed": list(map(lambda obj: {
+            "text": "{} vs. {}: {}-{}".format(obj["home"], obj["away"], obj["home_score"], obj["away_score"]),
+            "image": None
+        }, raw_json["mlb_scores"]["games"])),
+        "source": "http://api.sportradar.us/mlb/trial/v6.5/en/games/",
+        "expiration": None
+    }
+    parsed["nfl_scores"] = {
+        "feed": list(map(lambda obj: {
+            "text": "{} vs. {}: {}-{}".format(obj["home"], obj["away"], obj["home_score"], obj["away_score"]),
+            "image": None
+        }, raw_json["nfl_scores"]["games"])),
+        "source": "http://api.sportradar.us/nfl/official/trial/v5/en/games/",
+        "expiration": None
+    }
+
+    with open("./widgetfiles/parsed_widget_data.json", "w+") as f:
+        json.dump(parsed, f)
+
+    return raw_json
+
+print("output: ", fetch_info())
