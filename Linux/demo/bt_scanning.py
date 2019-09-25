ec51c42a0d6b25aa16dffb441de7b9283baa8a4c
Nick Thobe <nthobe@waive.com>
2019-09-25 02:10:41 -0700

    Testing of BT scanning

---
+++ Linux/demo/bt_scanning.py
@@ -0,0 +1,20 @@
+from scapy.layers.bluetooth import *
+from scapy.layers import bluetooth4LE, bluetooth
+
+#  First we need to initialize the usb module with: sudo hciconfig hci1 up
+
+def bt_packet_handler(packet):
+  p = packet[3][1]
+  #bt_packet_q.put(p)
+  print('{} @ {}dBi - {} {}'.format(p.addr, p.rssi, p.type, p.atype))
+
+bt = BluetoothHCISocket(1)
+bt.sr(HCI_Hdr() / HCI_Command_Hdr() / HCI_Cmd_LE_Set_Scan_Parameters(type=1))
+bt.sr(HCI_Hdr() / HCI_Command_Hdr() / HCI_Cmd_LE_Set_Scan_Enable(enable=True, filter_dups=False))
+try:
+  adverts = bt.sniff(lfilter=lambda p: HCI_LE_Meta_Advertising_Reports in p, prn=bt_packet_handler)
+except:
+  pass
+bt.sr(HCI_Hdr() / HCI_Command_Hdr() / HCI_Cmd_LE_Set_Scan_Enable(enable=False))
+
+
