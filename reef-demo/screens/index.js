fb24b258aaa08118029942c1fe2c251717224253
David Alexander Leigh <daleighan@gmail.com>
2019-09-26 12:01:37 -0700

    Fetching address info for screens

--- reef-demo/screens/index.js
+++ reef-demo/screens/index.js
@@ -1,23 +1,42 @@
 function renderScreens(screens, locations) {
-  screens.sort((a, b) => a.phone ? -1 : 1)
-  const screenTableBody = document.getElementById('screen-table-body');
-  screenTableBody.innerHTML = screens
-    .map(
-      screen =>
-        `
+  screens.sort((a, b) => {
+    if (a.lat && !b.lat) {
+      return -1;
+    }
+    if (b.lat && !a.lat) {
+      return 1;
+    }
+    return;
+  });
+  screens = screens.slice(0, 20);
+  let latLngs = '';
+  screens.forEach(screen => (latLngs += `[${screen.lat}, ${screen.lng}],`));
+  latLngs = latLngs.slice(0, latLngs.length - 1);
+  fetch(`http://192.168.86.58/api/location?multi=[${latLngs}]`)
+    .then(response => response.json())
+    .then(json => {
+      const screenTableBody = document.getElementById('screen-table-body');
+      screenTableBody.innerHTML = screens
+        .map(
+          (screen, i) =>
+            `
         <tr>
-          <th><a href="show?id=${screen.id}">${screen.id}</a></th>
+          <th><a href="show?id=${screen.id}">${screen.uid.slice(
+              screen.uid.length - 4,
+            )}</a></th>
           <th>${screen.project}</th>
-          <th>${screen.lat}, ${screen.lng}</th>
+          <th>${json[i]}</th>
           <th>${screen.phone}</th>
         </tr>
       `,
-    )
-    .join('');
+        )
+        .join('');
+    });
 }
 (() => {
   const screens = [];
   const locations = [];
+
   fetch('http://192.168.86.58/api/screens')
     .then(response => response.json())
     .then(json => screens.push(...json))
@@ -25,27 +44,11 @@ function renderScreens(screens, locations) {
       renderScreens(screens);
       self._map = map({points: screens});
       let success = false;
-      if(success) {
+      if (success) {
         _map.load(_campaign.shape_list);
       } else {
-        _map.center([-118.34,34.06], 11);
+        _map.center([-118.34, 34.06], 11);
       }
-      // The fetching of locations currently doesn't work due to cors issues. Return to this later if time
-      /*
-      return fetch(
-        `https://basic.waivecar.com/location.php?multi=[${screens.map(
-          screen => `[${[screen.lat, screen.lng]}]`,
-        )}]`,
-      );*/
     })
-    /*
-    .then(streets => {
-      return streets.json();
-    })
-    .then(json => {
-      locations.push(...json);
-      renderScreens(screens, locations);
-    })
-    */
     .catch(e => console.log('error fetching screens', e));
 })();
