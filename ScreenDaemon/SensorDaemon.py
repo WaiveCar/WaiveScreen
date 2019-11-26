cdb2aace3aea4070fdc9c55573d4527849291add
Chris McKenzie <kristopolous@yahoo.com>
2019-11-25 16:40:17 -0800

    storing the bootcount as arduino_seen

--- ScreenDaemon/SensorDaemon.py
+++ ScreenDaemon/SensorDaemon.py
@@ -104,7 +104,7 @@ while True:
 
       logging.info("Got first arduino read")
       first = False
-      db.kv_set('arduino_seen', 1)
+      db.kv_set('arduino_seen', db.get_bootcount())
       db.kv_set('arduino_version', sensor.get('Sw_version'))
 
     if not _autobright_set:
