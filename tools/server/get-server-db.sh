084cd877574bb57187dc4e0bc13684f89c407607
Chris McKenzie <kristopolous@yahoo.com>
2019-12-18 11:30:21 -0800

    fixing a typo on the get server db

--- tools/server/get-server-db.sh
+++ tools/server/get-server-db.sh
@@ -5,9 +5,9 @@ sql=/tmp/backup.sql
 db=/var/db/waivescreen/main.db
 ssh waivescreen.com "sqlite3 $db .dump | bzip2 -c > $compress"
 rm -f $compress $db $sql
-scp -C waivescreen.com:$compress $compress
+scp -qC waivescreen.com:$compress $compress
 bunzip2 -d $compress 
 sudo sqlite3 $db < $sql
 sudo chown www-data.www-data $db
-sudo chomod 0666 $db
+sudo chmod 0666 $db
 rm $compress $sql
