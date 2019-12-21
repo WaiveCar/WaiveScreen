7eb4aa52f553508b97a31964a99bdf051779e714
Nick Thobe <nthobe@waive.com>
2019-12-20 18:39:17 -0800

    hardware encoding, find correct camera device

--- explore/aws_video_streaming/ces_video_stream.sh
+++ explore/aws_video_streaming/ces_video_stream.sh
@@ -3,10 +3,22 @@
 . ${HOME}/lib.sh
 
 CES_STREAM_CHANNEL_ID="${CES_STREAM_CHANNEL_ID:-5987969}"
-CES_CAPTURE_DEVICE="${CES_CAPTURE_DEVICE:-/dev/video8}"
 
 INPUT_ID="$(aws medialive describe-channel --channel-id ${CES_STREAM_CHANNEL_ID} | jq -r '.InputAttachments[0].InputId')"
 SECURITY_ID="$(aws medialive describe-input --input-id ${INPUT_ID} | jq -r '.SecurityGroups[0]')"
+
+set_capture_device() {
+  for v in $(grep SPCA6500 /sys/class/video4linux/video*/name | cut -d ':' -f 1); do
+    local cap_device="/dev/$(basename $(dirname $v))"
+    if $(v4l2-ctl --all -d "${cap_device}" | grep -q 'Format Video Capture:'); then
+      _log "Setting CES_CAPTURE_DEVICE to ${cap_device}"
+      export CES_CAPTURE_DEVICE="${cap_device}"
+      return 0
+    fi
+  done
+  _warn "Unable to find the camera for streaming. Make sure it's plugged in and turned on"
+  exit 1
+}
 
 ffmpeg_hw_encoding_enable() {
   ffmpeg -init_hw_device vaapi=foo:/dev/dri/renderD128
@@ -14,8 +26,14 @@ ffmpeg_hw_encoding_enable() {
 
 _ffmpeg_stream() {
   _info "Starting CES Stream..."
+  # Software encoding
   #ffmpeg -re -f v4l2 -video_size 1920x1080 -framerate 30 -i "${CES_CAPTURE_DEVICE}" -vf scale=-1:720 -preset ultrafast -tune zerolatency -profile:v main -level 3.1 -pix_fmt yuv420p -c:v libx264 -x264opts "keyint=60:no-scenecut" -maxrate 2.5M -bufsize 5M -map 0 -f rtp_mpegts -fec prompeg=l=5:d=20 "$@" &
-  ffmpeg -re -f v4l2 -video_size 1920x1080 -framerate 30 -input_format mjpeg -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "${CES_CAPTURE_DEVICE}" -vf 'scale_vaapi=format=nv12' -profile:v main -level 3.1 -c:v h264_vaapi -b:v 2.5M -maxrate 5M -map 0 -f rtp_mpegts -fec prompeg=l=5:d=20 "$@" &
+  # Hardware encoding
+  ffmpeg -re -f v4l2 -video_size 1920x1080 -framerate 30 -input_format mjpeg \
+          -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi \
+          -i "${CES_CAPTURE_DEVICE}" -vf 'scale_vaapi=format=nv12' \
+          -c:v h264_vaapi -profile 578 -b:v 2M -maxrate 3M -bufsize 5M \
+          -map 0 -f rtp_mpegts -fec prompeg=l=5:d=20 "$@" &
   local f_pid=$!
   set_event ces_live_stream $f_pid
   wait $f_pid
@@ -37,5 +55,6 @@ start_ces_video_stream() {
   done
 }
 
+set_capture_device
 ffmpeg_hw_encoding_enable
 start_ces_video_stream
