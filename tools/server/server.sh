aed6e23f28643141e510a23b403205db4e6db2c1
Chris McKenzie <kristopolous@yahoo.com>
2020-01-24 16:14:57 -0800

    more permissive permissions for the staging

--- tools/server/server.sh
+++ tools/server/server.sh
@@ -17,7 +17,7 @@ db_stage() {
   set -x
   ssh waivescreen.com "sqlite3 $DB .dump > /tmp/sql"
   scp waivescreen.com:/tmp/sql staging.waivescreen.com:/tmp/sql
-  ssh staging.waivescreen.com "sudo rm $DB; sudo sqlite3 $DB < /tmp/sql; sudo chown www-data.www-data $DB"
+  ssh staging.waivescreen.com "sudo rm $DB; sudo sqlite3 $DB < /tmp/sql; sudo chown www-data.www-data $DB; sudo chmod 0777 $DB"
 }
 
 if ! declare -f $1 > /dev/null; then
