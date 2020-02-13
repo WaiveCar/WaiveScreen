1d2951aec4d5bb5dbbbbf564547483c80ed233c6
Chris McKenzie <kristopolous@yahoo.com>
2020-02-12 16:39:47 -0800

    not bailing on no number

--- ScreenDisplay/display.js
+++ ScreenDisplay/display.js
@@ -18,7 +18,8 @@ window.onload = function init() {
   self.sms = document.getElementById('sms');
 
   ads.on('system', function(data) {
-    document.getElementsByClassName('info')[0].innerHTML = [data.number.slice(-7), data.uuid.slice(0,5)].join(' ');
+    var number = data.number ? data.number.slice(-7) : '??';
+    document.getElementsByClassName('info')[0].innerHTML = [number, data.uuid.slice(0,5)].join(' ');
   });
 
   ads.on('jobEnded', function() {
